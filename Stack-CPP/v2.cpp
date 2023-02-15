4153443edd7d87f3f75a2e56f3a7692363d37380
Ivan Karaberov <68911388+ivan-karaberov@users.noreply.github.com>
2023-02-16 00:36:26 +0300

    Update v2.cpp

--- Stack-CPP/v2.cpp
+++ Stack-CPP/v2.cpp
@@ -1,62 +1,110 @@
 #include <iostream>
 
-class Stack {
-	private:
-		typedef struct Obj{
-			int data;
-			Obj* next;
-		} obj;
-
-		obj* top = NULL;
-
-	public:
-		bool empty();
-		void push(int data);
-		int pop();
-		void invert();
+class Node{
+public:
+    int data;
+    Node* next;
+    Node(int data){
+        this->data = data;
+        this->next = NULL;
+    }
 };
 
+
+class LinkedList{
+private:
+    int size;
+    Node* head;
+public:
+    
+    LinkedList(){
+        size = 0;
+        head = NULL;
+    }
+
+    ~LinkedList(){
+        size = 0;
+        while(head != NULL) pop();
+    }
+
+    int getSize(){
+        return size;
+    }
+
+    int empty(){
+        return getSize() == 0;
+    }
+
+    void push(int data){
+        Node* ptr = new Node(data);
+        ptr->next = head;
+        head = ptr;
+        
+        size++;
+    }
+
+    void pop(){
+        if (head == NULL) return;
+
+        Node* ptr = head;
+        head = ptr->next;
+        delete ptr;
+
+        size--;
+    }
+
+    Node* getByIndex(int index){
+        Node* ptr = head;
+        int n = 0;
+
+        while(n != index){
+            if(ptr == NULL) return ptr;
+            ptr = ptr->next;
+            n++;
+        }
+
+        return ptr;
+    }
+
+    
+	Node* operator [] (int index) {
+		return  getByIndex(index);
+	}
+
+    void invert(){
+        for(Node* ptr = head; ptr != NULL; ptr = ptr->next){
+            ptr->data *= -1;
+        }
+    }
+
+    void swap(int index1, int index2){
+        Node* ptr_index1 = getByIndex(index1);
+        Node* ptr_index2 = getByIndex(index2);
+        if(ptr_index1 == NULL || ptr_index2 == NULL) return;
+        int tmp = ptr_index1->data;
+        ptr_index1->data = ptr_index2->data;
+        ptr_index2->data = tmp;
+    }
+
+    Node* top(){
+        return head;
+    }
+
+};
+
+
 int main(){
-	Stack c;
-	c.push(1);
-	c.push(2);
-	c.push(-3);
-	c.invert();
+    LinkedList lst;
+    lst.push(1);
+    lst.push(2);
+    lst.push(3);
 
-	while(!c.empty()){
-		std::cout << "Deleted > " << c.pop() << std::endl;
-	}
+    std::cout << ">> " << lst[1]->data  << std::endl;
+    std::cout << ">> " << lst[2]->data;
+    lst.swap(0, 2);
+    lst.invert();
+    lst.pop();
 
-	return 0;
-}
-
-
-bool Stack::empty(){
-	return top ? false : true;
-}
-
-void Stack::push(int data){
-	obj* stk = new obj;
-	stk -> data = data;
-	stk -> next = top;
-	top = stk;
-}
-
-int Stack::pop(){
-	if (empty()) return 0;
-	int tmp;
-	obj* stk = top;
-	tmp = stk -> data;
-	top = stk -> next;
-	delete stk;
-	return tmp;
-} 
-
-void Stack::invert(){
-	obj* stk = top;
-
-	while(stk){
-	stk->data = -(stk->data);
-	stk = stk->next;
-	}
+    std::cout << std::endl;
+    return 0;
 }
